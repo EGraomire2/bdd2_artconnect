2d4e2c6935d2c133e97e781face6319b690fb963
EGromaire <etienne.gromaire@efrei.net>
2026-05-05 15:32:18 +0200

    fix

--- src/main/java/com/project/artconnect/dao/MemberFavoriteDisciplineDao.java
+++ src/main/java/com/project/artconnect/dao/MemberFavoriteDisciplineDao.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface MemberFavoriteDisciplineDao {
     void addFavoriteDiscipline(int memberId, int disciplineId) throws SQLException;
+    
     void removeFavoriteDiscipline(int memberId, int disciplineId) throws SQLException;
+    
     List<Integer> getFavoriteDisciplineIdsByMemberId(int memberId);
+    
     List<Integer> getMemberIdsByDisciplineId(int disciplineId);
+    
     boolean exists(int memberId, int disciplineId);
 }
--- src/main/java/com/project/artconnect/dao/WorkshopDao.java
+++ src/main/java/com/project/artconnect/dao/WorkshopDao.java
@@ -1,16 +1,19 @@
 package com.project.artconnect.dao;
 
-import com.project.artconnect.model.Workshop;
 import java.util.List;
 import java.util.Optional;
+
+import com.project.artconnect.model.Workshop;
 
 public interface WorkshopDao {
     Optional<Workshop> findById(Long id);
     Optional<Workshop> findByTitle(String title);
 
-    void save(Workshop workshop);
-    void update(Workshop workshop);
-    void delete(Long id);
-    
     List<Workshop> findAll();
+
+    void save(Workshop workshop);
+
+    void update(Workshop workshop);
+
+    void delete(String title);
 }
