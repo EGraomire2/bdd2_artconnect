3a14a16e6ce7be4d1d33a1e23ce36cd60682ddda
BaturHamzaogullari <baturhamzaogullari@gmail.com>
2026-05-05 11:52:41 +0200

    CommuntyMemberDao

--- src/main/java/com/project/artconnect/dao/CommunityMemberDao.java
+++ src/main/java/com/project/artconnect/dao/CommunityMemberDao.java
@@ -5,7 +5,13 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommunityMemberDao {
-    Optional<CommunityMember> findById(Long id);
+    Optional<CommunityMember> findByName(String name);
 
     List<CommunityMember> findAll();
+
+    void save(CommunityMember member);
+
+    void update(CommunityMember member);
+
+    void delete(String name);
 }
