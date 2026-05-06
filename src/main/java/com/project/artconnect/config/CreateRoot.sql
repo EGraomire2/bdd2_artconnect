b0d9043dfa6bfb6518e11989a9dc13c7faf37374
EGromaire <etienne.gromaire@efrei.net>
2026-05-06 09:59:42 +0200

    Connection BDD

--- src/main/java/com/project/artconnect/config/CreateRoot.sql
+++ src/main/java/com/project/artconnect/config/CreateRoot.sql
@@ -1,9 +1,22 @@
--- Créer l'utilisateur
-CREATE USER 'ArtconnectRoot'@'localhost' IDENTIFIED BY 'R00TSAF3p$ssw0rd';
+-- Nettoyer les anciens users (au cas où)
+DROP USER IF EXISTS 'ArtconnectRoot'@'localhost';
+DROP USER IF EXISTS 'ArtconnectRoot'@'127.0.0.1';
+
+-- Créer l'utilisateur avec le mot de passe ArtConnect_2026_Secure
+CREATE USER 'ArtconnectRoot'@'localhost' IDENTIFIED BY 'ArtConnect_2026_Secure';
 
 -- Accorder tous les droits sur la base de données artconnect
 GRANT ALL PRIVILEGES ON artconnect.* TO 'ArtconnectRoot'@'localhost';
 
 -- Appliquer les changements
 FLUSH PRIVILEGES;
+
+
+-- Vérifier les privileges de l'utilisateur
 SHOW GRANTS FOR 'ArtconnectRoot'@'localhost';
+
+-- IDENTIFIANTS:
+--    - Username: ArtconnectRoot
+--    - Password: ArtConnect_2026_Secure
+--    - Host: localhost
+--    - Database: artconnect
